9e01201e045536382bcf78b774ad1c51e0e1da17
EMish <Liza.s.mishina@gmail.com>
2020-07-31 17:52:07 -0700

    3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -6,26 +6,18 @@ import csv
 #retrieve data
 file_to_load = os.path.join('Resource', 'election_results.csv')
 
-with open(file_to_load) as election_data:
-
-    print(election_data)
-
-# Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
 
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+with open(file_to_load) as election_data:
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    # Write three counties to the file.
-     txt_file.write("Counties in the Elections\n--------------------\nArapahoe\nDenver\nJefferson")
+#read the csv
+    file_reader = csv.reader(election_data)
 
-
-
+    #read+print header row
+    headers = next(file_reader)
+    print(headers)
 
 
 #Total number of votes cast
@@ -36,3 +28,32 @@ with open(file_to_save, "w") as txt_file:
 #process data
 #Percentage of votes each candidate won
 #The winner of the election based on popular vote
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create a filename variable to a direct or indirect path to the file.
+#file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+    # Write three counties to the file.
+     #txt_file.write("Counties in the Elections\n--------------------\nArapahoe\nDenver\nJefferson")
+
+
+
+    
+
+
+
